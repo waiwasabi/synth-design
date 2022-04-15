d7e53b3dce5ee62468a387355904ca0cacb36b2a
WaiWasabi <aarontianqwerty@outlook.com>
2022-04-15 16:07:43 -0400

    4/15/22: laptop work on new environment

--- src/main/new_environment.py
+++ src/main/new_environment.py
@@ -2,20 +2,48 @@ import torch
 import numpy as np
 
 from src.utils.reservoir_sample import sample_file
-
+from rdkit.Chem.rdChemReactions import ReactionFromSmarts
+from rdkit.Chem import MolFromSmiles
+from collections import deque
 
 class Node:
-    def __init__(self, value, children=None):
-        self.value = value
+    def __init__(self, smiles, children=None):
+        self.smiles = smiles
+        self.mol_wrapper = MolWrapper(smiles)
         self.children = [] if children is None else children
 
     def get_children(self):
         return self.children
 
 
+class ReactionWrapper:
+    def __init__(self, smarts):
+        self.smarts = smarts
+        self.reaction = ReactionFromSmarts(smarts)
+        self.reaction.initialize()
+
+    def run_reactants(self, reactants):
+        try:
+            return self.reaction.runReactants(reactants)
+        except:
+            return None
+
+
+class MolWrapper:
+    def __init__(self, smiles):
+        self.smiles = smiles
+        self.mol = None if smiles is None else MolFromSmiles(smiles)
+
+    def __str__(self):
+        return self.smiles
+
+
 class BinaryTree:
     def __init__(self, root):
         self.root = root
+
+    def add(self, mol_wrapper):
+        pass
 
 
 class BuildingBlockDataset:
@@ -28,6 +56,7 @@ class BuildingBlockDataset:
 
     def calculate_fp(self, nbits):
         pass
+
 
 class SynthesisFramework:
     def __init__(self, target, objectives):
@@ -44,3 +73,5 @@ class SynthesisFramework:
         self.t += 1
 
 
+if __name__ == '__main__':
+    print(MolWrapper(None))
